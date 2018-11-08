1073d4a382b467f60fb626d32d8730d4b9eda2fd
Jean THOMAS <pub0@github.jeanthomas.me>
2018-11-08 16:17:01 +0100

    Create SketchESP8266.ino

---
+++ SketchESP8266.ino
@@ -0,0 +1,94 @@
+/*
+  WiFiTelnetToSerial - Example Transparent UART to Telnet Server for esp8266
+
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266WiFi library for Arduino environment.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <ESP8266WiFi.h>
+
+//how many clients should be able to telnet to this ESP8266
+#define MAX_SRV_CLIENTS 1
+const char* ssid = "**********";
+const char* password = "**********";
+
+WiFiServer server(23);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
+IPAddress local_IP(10,0,0,1);
+IPAddress gateway(10,0,0,10);
+IPAddress subnet(255,0,0,0);
+
+void setup() {
+  Serial1.begin(115200);
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  WiFi.softAP("Polybot1");
+  
+  //start UART and the server
+  Serial.begin(115200);
+  server.begin();
+  server.setNoDelay(true);
+}
+
+void loop() {
+  uint8_t i;
+  //check if there are any new clients
+  if (server.hasClient()) {
+    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+      //find free/disconnected spot
+      if (!serverClients[i] || !serverClients[i].connected()) {
+        if (serverClients[i]) {
+          serverClients[i].stop();
+        }
+        serverClients[i] = server.available();
+        Serial1.print("New client: "); Serial1.print(i);
+        break;
+      }
+    }
+    //no free/disconnected spot so reject
+    if (i == MAX_SRV_CLIENTS) {
+      WiFiClient serverClient = server.available();
+      serverClient.stop();
+      Serial1.println("Connection rejected ");
+    }
+  }
+  //check clients for data
+  for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+    if (serverClients[i] && serverClients[i].connected()) {
+      if (serverClients[i].available()) {
+        //get data from the telnet client and push it to the UART
+        while (serverClients[i].available()) {
+          Serial.write(serverClients[i].read());
+        }
+      }
+    }
+  }
+  //check UART for data
+  if (Serial.available()) {
+    size_t len = Serial.available();
+    uint8_t sbuf[len];
+    Serial.readBytes(sbuf, len);
+    //push UART data to all connected telnet clients
+    for (i = 0; i < MAX_SRV_CLIENTS; i++) {
+      if (serverClients[i] && serverClients[i].connected()) {
+        serverClients[i].write(sbuf, len);
+        delay(1);
+      }
+    }
+  }
+}
